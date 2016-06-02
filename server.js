bb2472c6490a1953df15da30bcaf583c20af6758
luisfpinto <luisfpintos@gmail.com>
2016-06-02 16:11:04 +0200

    First commit

--- server.js
+++ server.js
@@ -17,6 +17,19 @@
 /* Requires node.js libraries */
 var express = require('express')
 var app = express()
+//Upload files
+var multer  = require('multer')
+var upload = multer({ dest: 'uploads/' })
+
+//Netbeast API
+var netbeast = require('netbeast')
+
+var bodyParser = require('body-parser')
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+})); 
+
 
 // Netbeast apps need to accept the port to be launched by parameters
 var argv = require('minimist')(process.argv.slice(2))
@@ -27,4 +40,15 @@ var server = app.listen(argv.port || 31416, function () {
   var host = server.address().address
   var port = server.address().port
   console.log('Example app listening at http://%s:%s', host, port)
+})
+
+app.post('/upload',upload.single('track'), function (req,res) {
+
+	var originalname = req.file.originalname
+	var mimetype = req.file.mimetype
+	var path = req.file.path
+	console.log(req.file)
+
+	if(mimetype !== 'audio/mp3') return res.status(406).send('File format not acceptable')
+
 })
